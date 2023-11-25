b0c3f89428356e95ad0469586d57b5e4756c8be3
mfadhili <mfadhili@github.com>
2023-11-26 02:50:31 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function App(props) {
             <Avatar />
             <div className="data">
                 <Intro />
-                <SkillList />
+                <SkillList skills={skills}/>
             </div>
         </div>
     );
@@ -33,35 +33,66 @@ function Avatar(props) {
 
 function Intro(props) {
     return (
-        <div>
+        <div >
             <h1>Steps to Starting a Business While Working Full Time</h1>
             <p>Building a startup company can be an exciting and rewarding experience, but it also requires a lot of hard work and dedication.</p>
             <p>Did you know 3.2 million people in the United Kingdom have aspirations to establish their own small business?</p>
             <p>That's 10% of the country's working population and a significant portion of the current job market. The difficulty is that most people are unable to leave their full-time jobs to start a business. According to BalanceSMB, a company's first profit can take anywhere from six months to several years. With that concept weighing heavily on many people's minds, you may be hesitant to start your own firm.</p>
-            <p>It is possible to establish a business while juggling the responsibilities of full-time employment. This is something that many businesses are successful at. You can start developing a company idea wIt is possible to establish a business while juggling the responsibilities of full-time employment. This is something that many businesses are successful at. You can start developing a company idea while still working your full-time job and preserving that all-important financial safety net with a side business, which is sometimes referred to as a side hustle.hile still working your full-time job and preserving that all-important financial safety net with a side business, which is sometimes referred to as a side hustle.</p>
+            <p>It is possible to establish a business while juggling the responsibilities of full-time employment. This is something that many businesses are successful at.</p>
+            <p>You can start developing a company idea while still working your full-time job and preserving that all-important financial safety net with a side business, which is sometimes referred to as a side hustle. While still working your full-time job and preserving that all-important financial safety net with a side business, which is sometimes referred to as a side hustle.</p>
         </div>
     );
 }
 
-function SkillList(props) {
+function SkillList({skills}) {
     return (
         <div className="skill-list">
-            <Skill skill="Time Management" emoji="💪" color="cyan" />
-            <Skill skill="Team work" emoji="💪" color="orangered" />
-            <Skill skill="Vision" emoji="💪" color="yellow" />
-            <Skill skill="Consistency" emoji="💪" color="green" />
-            <Skill skill="Money Making" emoji="💪" color="blue" />
+            {
+                skills.map((skill) => {
+                    return <Skill skill={skill.skill} color={skill.color} level={skill.level}/>
+                })
+            }
         </div>
     );
 
 }
 
-function Skill(props) {
+function Skill({skill, color, level}) {
     return (
-        <div className="skill" style={{backgroundColor: props.color}}>
-            <span>{props.skill}</span>
-            <span>{props.emoji}</span>
+        <div className="skill" style={{backgroundColor: color}}>
+            <span>{skill}</span>
+            <span>
+                {skill === 'essential' && "💪"}
+                {skill === 'good to have' && "👍"}
+            </span>
         </div>
     );
 }
 
+const skills = [
+    {
+        skill: "Time Management",
+        level: "essential",
+         color: "cyan",
+    },
+    {
+        skill: "Team work",
+        level: "essential",
+        color: "orangered",
+    },
+    {
+        skill: "Vision",
+        level: "essential",
+        color: "yellow",
+    },
+    {
+        skill: "Consistency",
+        level: "essential",
+        color: "green",
+    },
+    {
+        skill: "Money Making",
+        level: "good to have",
+        color: "blue",
+    },
+];
